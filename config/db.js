5e62b944f429de2d52e07c9eec8280867759cc5e
AnumZainab999 <anumzainab999@gmail.com>
2025-08-20 18:07:15 +0500

    hm

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'mysql',
+    dialect: process.env.DB_DIALECT || 'mysql2',
     logging: process.env.DB_LOGGING === 'true' ? console.log : false,
     define: { underscored: false, freezeTableName: false },
     pool: { max: 10, min: 0, acquire: 30000, idle: 10000 }
